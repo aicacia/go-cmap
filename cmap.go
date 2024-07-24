f18e88ea2705e74c7be4b1ff8d1dec433766a580
Nathan Faucett <nathanfaucett@gmail.com>
2024-07-24 18:46:30 -0400

    make inherit private, add LoadOrStore

--- cmap.go
+++ cmap.go
@@ -6,7 +6,7 @@ import (
 )
 
 type CMap[K, V any] struct {
-	sync.Map
+	m     sync.Map
 	count atomic.Int64
 }
 
@@ -15,7 +15,7 @@ func New[K, V any]() CMap[K, V] {
 }
 
 func (m *CMap[K, V]) SetIfAbsent(key K, value V) bool {
-	if _, isOld := m.Map.LoadOrStore(key, value); !isOld {
+	if _, isOld := m.m.LoadOrStore(key, value); !isOld {
 		m.count.Add(1)
 		return true
 	} else {
@@ -24,7 +24,7 @@ func (m *CMap[K, V]) SetIfAbsent(key K, value V) bool {
 }
 
 func (m *CMap[K, V]) Set(key K, value V) bool {
-	if _, isOld := m.Map.Swap(key, value); !isOld {
+	if _, isOld := m.m.Swap(key, value); !isOld {
 		m.count.Add(1)
 		return true
 	} else {
@@ -33,7 +33,7 @@ func (m *CMap[K, V]) Set(key K, value V) bool {
 }
 
 func (m *CMap[K, V]) Has(key K) bool {
-	_, ok := m.Map.Load(key)
+	_, ok := m.m.Load(key)
 	return ok
 }
 
@@ -42,23 +42,29 @@ func (m *CMap[K, V]) IsEmpty() bool {
 }
 
 func (m *CMap[K, V]) Get(key K) (V, bool) {
-	if value, ok := m.Map.Load(key); ok {
+	if value, ok := m.m.Load(key); ok {
 		return value.(V), true
 	} else {
 		return *new(V), false
 	}
 }
 
-func (m *CMap[K, V]) GetOrSet(key K, value V) V {
-	result, isOld := m.Map.LoadOrStore(key, value)
+func (m *CMap[K, V]) LoadOrStore(key K, value V) (result V, isOld bool) {
+	actual, isOld := m.m.LoadOrStore(key, value)
 	if !isOld {
 		m.count.Add(1)
 	}
-	return result.(V)
+	result = actual.(V)
+	return result, isOld
+}
+
+func (m *CMap[K, V]) GetOrSet(key K, value V) V {
+	result, _ := m.LoadOrStore(key, value)
+	return result
 }
 
 func (m *CMap[K, V]) Delete(key K) bool {
-	if _, isOld := m.Map.LoadAndDelete(key); isOld {
+	if _, isOld := m.m.LoadAndDelete(key); isOld {
 		m.count.Add(-1)
 		return true
 	} else {
@@ -71,7 +77,7 @@ func (m *CMap[K, V]) Remove(key K) bool {
 }
 
 func (m *CMap[K, V]) Range(f func(key K, value V) bool) {
-	m.Map.Range(func(key, value any) bool {
+	m.m.Range(func(key, value any) bool {
 		return f(key.(K), value.(V))
 	})
 }
@@ -84,7 +90,7 @@ type Entry[K, V any] struct {
 func (m *CMap[K, V]) Iter() chan Entry[K, V] {
 	ch := make(chan Entry[K, V])
 	go func() {
-		m.Map.Range(func(key, value any) bool {
+		m.m.Range(func(key, value any) bool {
 			ch <- Entry[K, V]{
 				Key: key.(K),
 				Val: value.(V),
@@ -99,7 +105,7 @@ func (m *CMap[K, V]) Iter() chan Entry[K, V] {
 func (m *CMap[K, V]) Keys() chan K {
 	ch := make(chan K)
 	go func() {
-		m.Map.Range(func(key, _ any) bool {
+		m.m.Range(func(key, _ any) bool {
 			ch <- key.(K)
 			return true
 		})
@@ -111,7 +117,7 @@ func (m *CMap[K, V]) Keys() chan K {
 func (m *CMap[K, V]) Values() chan V {
 	ch := make(chan V)
 	go func() {
-		m.Map.Range(func(_, value any) bool {
+		m.m.Range(func(_, value any) bool {
 			ch <- value.(V)
 			return true
 		})
@@ -129,7 +135,7 @@ func (m *CMap[K, V]) Len() int {
 }
 
 func (m *CMap[K, V]) Clear() {
-	m.Map.Range(func(key, _ any) bool {
+	m.m.Range(func(key, _ any) bool {
 		m.Delete(key.(K))
 		return true
 	})
